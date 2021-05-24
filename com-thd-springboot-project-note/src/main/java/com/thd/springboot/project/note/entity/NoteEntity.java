fa0524d348b5045cc994cb3fd5cbbcaedab96db9
十三妖 <181907667@qq.com>
2021-05-24 22:02:04 +0800

    note

--- com-thd-springboot-project-note/src/main/java/com/thd/springboot/project/note/entity/NoteEntity.java
+++ com-thd-springboot-project-note/src/main/java/com/thd/springboot/project/note/entity/NoteEntity.java
@@ -3,9 +3,20 @@ package com.thd.springboot.project.note.entity;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class NoteEntity extends NoteEntityParent {
 	private static final long serialVersionUID = 1L;
 	private String highLight;
+
+	private Date expireDateFrom;
+	private Date expireDateTo;
+
+	private Date finishTimeFrom;
+	private Date finishTimeTo;
+
+	private Date startTimeFrom;
+	private Date startTimeTo;
 }
 
