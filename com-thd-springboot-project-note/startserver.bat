4c4829449fe7cdfa4f381e6e4ab0f127da2af94a
wang lei <wanglei62@lenovo.com>
2021-05-25 17:06:50 +0800

    Create startserver.bat

---
+++ com-thd-springboot-project-note/startserver.bat
@@ -0,0 +1,9 @@
+title Note
+
+start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:5678
+
+cd C:\Users\wanglei62\Desktop\web-service\local\note
+java -jar com-thd-springboot-project-note-1.0-SNAPSHOT.jar --server.port=5678
+
+
+ 
