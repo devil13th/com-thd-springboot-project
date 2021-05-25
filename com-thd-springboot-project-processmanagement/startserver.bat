b34c253746118c0444a6388be483e7531b23e9d2
wang lei <wanglei62@lenovo.com>
2021-05-25 17:07:27 +0800

    Create startserver.bat

---
+++ com-thd-springboot-project-processmanagement/startserver.bat
@@ -0,0 +1,9 @@
+title Process Management System
+
+start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:2346/index.html 
+
+cd C:\Users\wanglei62\Desktop\web-service\local\processManager
+java -jar com-thd-springboot-project-processmanagement-1.0-SNAPSHOT.jar
+
+
+ 
